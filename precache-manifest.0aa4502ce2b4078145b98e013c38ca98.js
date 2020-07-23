833aea753326e781a029dd292f2beb89f81ceadb
vkolintso <v.kolintso@theomnicore.com>
2020-07-23 17:50:11 +0300

    Updates

---
+++ precache-manifest.0aa4502ce2b4078145b98e013c38ca98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ae162b73ed40430ce662217d21320c",
+    "url": "/yalantis/index.html"
+  },
+  {
+    "revision": "04dcb7b5b25c3be00a47",
+    "url": "/yalantis/static/css/main.b53c0ce4.chunk.css"
+  },
+  {
+    "revision": "88cc139ad1d118d84a41",
+    "url": "/yalantis/static/js/2.12e5b6d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yalantis/static/js/2.12e5b6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dcb7b5b25c3be00a47",
+    "url": "/yalantis/static/js/main.4e3420e5.chunk.js"
+  },
+  {
+    "revision": "3e4add2e14b9c75276fc",
+    "url": "/yalantis/static/js/runtime-main.c97a4e3a.js"
+  }
+]);
